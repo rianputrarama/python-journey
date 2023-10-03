bb09fc96b4c30a16cd424beda0e64397b3c4473a
veonicarin <veonicarin@sangraz>
2023-10-04 02:34:10 +0700

    using underscore to handle whatever client write

--- match-case.py
+++ match-case.py
@@ -2,14 +2,19 @@ todos = []
 
 while True:
     action = input("Type add, show and exit : ")
+    action = action.strip()
 
     match action:
         case 'add':
             todo = input("Enter a todo : ")
             todos.append(todo.capitalize())
         case 'show':
-            print(todos)
+            for item in todos:
+                item = item.title()
+                print(item)
         case 'exit':
             break
+        case _:
+            print("you entered an unknown command")
 
 print("bye!")
